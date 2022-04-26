964779d4322a4192f3fee4515b5b9b9651f74bf9
KarlisPurins <karlis.purins.1999@gmail.com>
2022-04-26 22:41:11 +0300

    Added Character movement and Hammer

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+
+    }
+}
